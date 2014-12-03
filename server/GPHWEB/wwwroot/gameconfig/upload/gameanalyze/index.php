efaada6cc5caeeb949736cf6210b9a0db9a78b26
Denis <wmzaru@gmail.com>
2014-12-03 13:51:21 +0200

    Загрузка исходника на сервер

---
+++ server/GPHWEB/wwwroot/gameconfig/upload/gameanalyze/index.php
@@ -0,0 +1,2 @@
+fuck you !!!!!!!! this place by QQ231734839£¡ 
+and you can not get anything . so get out !!hurry up !
---
+++ server/GPHWEB/wwwroot/gameconfig/upload/index.php
@@ -0,0 +1,2 @@
+fuck you !!!!!!!! this place by QQ231734839 ª® ¨’€ £° 
+and you can not get anything . so get out !!hurry up !
